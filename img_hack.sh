8c40a3001b34e6db571cc0d83b9d06be8f970b69
vmcquinn <vmcquinn@email.sc.edu>
2018-02-17 04:19:36 -0500

    docs push

--- img_hack.sh
+++ img_hack.sh
@@ -1,8 +1,8 @@
 mv dist docs
 cp index.html docs
 
-sed -i -e 's/\/dist\/main.css/dist\/main.css/g' docs/index.html
-sed -i -e 's/\/dist\/build.js/dist\/build.js/g' docs/index.html
+sed -i -e 's/\/dist\/main.css/main.css/g' docs/index.html
+sed -i -e 's/\/dist\/build.js/build.js/g' docs/index.html
 
 
 sed -i -e 's/n.p+"si-glyph-arrow-thick-thin-down.svg/n.p+"si-glyph-arrow-thick-thin-down.svg/g' docs/build.js
