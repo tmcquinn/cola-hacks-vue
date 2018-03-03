15f1c4519b5d2cca1394cc3b6277009a55c70d39
Natalie Davidson <davidsonnatalies@gmail.com>
2018-03-02 20:56:07 -0500

    WARNING DONT PUSH THIS THIS IS MY D3 SHIT U COPIED THE DIRECTORY

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,11 +17,7 @@
         href="https://twitter.com/ayylilnatto"
     data-size="large">
     Follow @ColaHacks</a>
-          <div class="author col-lg-4 col-md-4 col-sm-12"><span class="bold">
-            Keep up with ColaHacks.
-            <div class="fb-page" data-href="https://www.facebook.com/uofsccolahacks" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/uofsccolahacks" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/uofsccolahacks">ColaHacks</a></blockquote></div>
-            //Twitter Icon Here
-          </span></div>
+            <div class="fb-page col-lg-4 col-md-4 col-sm-12" data-href="https://www.facebook.com/uofsccolahacks" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/uofsccolahacks" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/uofsccolahacks">ColaHacks</a></blockquote></div>
     </div>
 </template>
 
