63f50d3f551794f0245e240480c8c9e1db5b8a52
vmcquinn <vmcquinn@email.sc.edu>
2018-03-21 14:10:20 -0400

    resizing FAQ

--- src/components/ColaFAQ.vue
+++ src/components/ColaFAQ.vue
@@ -90,7 +90,7 @@ export default {
 </script>
 
 <style>
-    @media screen and (max-width: 600px) {
+    @media screen and (max-width: 900px) {
         .white-bg {
             flex-direction: column !important;
         }
