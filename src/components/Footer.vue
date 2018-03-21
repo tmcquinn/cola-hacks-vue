6057560352076c479dacdd8481fb12ddbdcbe71e
vmcquinn <vmcquinn@email.sc.edu>
2018-03-21 14:08:51 -0400

    attempt 2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,33 +4,19 @@
             <p>Questions, comments? Contact us!
             <br>colahacks@gmail.com
             </p>
-<<<<<<< HEAD
         </div>
         <div class="fact col-lg-4 col-md-4 col-sm-12">
-=======
-          </div>
-          <div class="fact col-lg-4 col-md-4 col-sm-12">
->>>>>>> 0f222cb86d3d50c4afecdfe20975cd53de3c1134
             <b-link href="#info">info</b-link><br>
             <b-link href="#about">about us</b-link><br>
             <b-link href="#register">register</b-link><br>
             <b-link href="#faq">faq</b-link><br>
             <b-link href="#schedule">schedule</b-link><br>
-<<<<<<< HEAD
         </div>
         <a class="twitter-follow-button"
         href="https://twitter.com/colahacks"
         data-size="large">
         Follow @ColaHacks</a>
         <a href="https://www.facebook.com/uofsccolahacks">Follow Us on Facebook!</a>
-=======
-          </div>
-          <a class="twitter-follow-button"
-        href="https://twitter.com/ayylilnatto"
-    data-size="large">
-    Follow @ColaHacks</a>
-            <div class="fb-page col-lg-4 col-md-4 col-sm-12" data-href="https://www.facebook.com/uofsccolahacks" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/uofsccolahacks" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/uofsccolahacks">ColaHacks</a></blockquote></div>
->>>>>>> 0f222cb86d3d50c4afecdfe20975cd53de3c1134
     </div>
 </template>
 
