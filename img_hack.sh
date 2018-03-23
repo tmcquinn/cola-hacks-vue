0ddb60568ec0b99d6bfbc728aaf5a6a8ece17221
Tori McQuinn <tmcquinn@swampfox.com>
2018-03-23 12:44:24 -0400

    adding img fix

--- img_hack.sh
+++ img_hack.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cp docs/CNAME .
 
 rm -rf docs
@@ -11,5 +12,8 @@ cp index.html docs
 sed -i -e 's/\/dist\/main.css/main.css/g' docs/index.html
 sed -i -e 's/\/dist\/build.js/build.js/g' docs/index.html
 
+# Need to rename all photonames
 sed -i -e 's/n.p+"si-glyph-arrow-thick-thin-down.svg/"si-glyph-arrow-thick-thin-down.svg/g' docs/build.js
 sed -i -e 's/n.p+"logo.svg/"logo.svg/g' docs/build.js
+sed -i -e 's/n.p+"sf-logo.png/"sf-logo.png/g' docs/build.js
+sed -i -e 's/n.p+"52inc.png/"52inc.png/g' docs/build.js
